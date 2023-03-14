62d8d2a498f2548efadff0987b03464d3307e9c3
nadezhda <abuziarova.nadezhda@gmail.com>
2023-03-14 22:06:36 +0300

    Solution 1

--- Program/Program.cs
+++ Program/Program.cs
@@ -5,6 +5,7 @@
 // При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 
+
 string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
 
